4e966ae9408ed0a03c278538856a9c3f0c12cff3
Uri Shaked <uri@urishaked.com>
2019-01-13 00:35:40 +0200

    feat(zephyr): set up NeoPixel driver

--- zephyr/app/src/main.c
+++ zephyr/app/src/main.c
@@ -1,25 +1,62 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <string.h>
+
+#define LOG_LEVEL 4
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main);
+
 #include <zephyr.h>
 #include <device.h>
 #include <gpio.h>
+#include <led_strip.h>
+#include <spi.h>
+#include <misc/util.h>
 
 #define LED_PORT LED0_GPIO_CONTROLLER
 #define LED	LED0_GPIO_PIN
 
+#define STRIP_NUM_LEDS 2
+#define STRIP_DEV_NAME CONFIG_WS2812_STRIP_NAME
+
 #define SLEEP_TIME 	500
 
-void main(void)
-{
-	int cnt = 0;
-	struct device *dev;
+static const struct led_rgb red = { .r = 0x20, .g = 0x00, .b = 0x00 };
+static const struct led_rgb green = { .r = 0x0, .g = 0x20, .b = 0x0 };
 
-	dev = device_get_binding(LED_PORT);
-	/* Set LED pin as output */
-	gpio_pin_configure(dev, LED, GPIO_DIR_OUT);
+struct led_rgb strip_colors[STRIP_NUM_LEDS];
+
+void main(void) {
+	struct device *strip;
+	struct device *gpio;
+
+	gpio = device_get_binding(LED_PORT);
+	gpio_pin_configure(gpio, LED, GPIO_DIR_OUT);
+
+
+    strip = device_get_binding(STRIP_DEV_NAME);
+    if (!strip) {
+		LOG_ERR("LED strip device %s not found", STRIP_DEV_NAME);
+		return;
+	}
 
 	while (1) {
-		/* Set pin to HIGH/LOW two times a second */
-		gpio_pin_write(dev, LED, cnt % 2);
-		cnt++;
+		gpio_pin_write(gpio, LED, 0);
+		strip_colors[0] = red;
+		strip_colors[1] = green;
+		led_strip_update_rgb(strip, strip_colors, STRIP_NUM_LEDS);
+		k_sleep(SLEEP_TIME);
+
+		gpio_pin_write(gpio, LED, 1);
+		strip_colors[0] = green;
+		strip_colors[1] = red;
+		led_strip_update_rgb(strip, strip_colors, STRIP_NUM_LEDS);
 		k_sleep(SLEEP_TIME);
 	}
 }
