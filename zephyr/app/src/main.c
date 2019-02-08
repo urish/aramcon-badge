ccee12528067e95bb095adf41f992c3bd7dfdfaf
Uri Shaked <uri@urishaked.com>
2019-02-09 01:19:58 +0200

    feat(zephyr): add epaper device
    seems to be half-working on the waveshare module

--- zephyr/app/src/main.c
+++ zephyr/app/src/main.c
@@ -16,7 +16,7 @@ LOG_MODULE_REGISTER(main);
 #include <device.h>
 #include <gpio.h>
 #include <led_strip.h>
-#include <spi.h>
+#include <display/cfb.h>
 #include <misc/util.h>
 
 #define LED_PORT LED0_GPIO_CONTROLLER
@@ -27,6 +27,8 @@ LOG_MODULE_REGISTER(main);
 
 #define SLEEP_TIME 	500
 
+#define DISPLAY_DRIVER		"SSD1673"
+
 static const struct led_rgb red = { .r = 0x20, .g = 0x00, .b = 0x00 };
 static const struct led_rgb green = { .r = 0x0, .g = 0x20, .b = 0x0 };
 static const struct led_rgb blue = { .r = 0x0, .g = 0x0, .b = 0x20 };
@@ -34,12 +36,15 @@ static const struct led_rgb purple = { .r = 0x10, .g = 0x0, .b = 0x20 };
 
 struct led_rgb strip_colors[STRIP_NUM_LEDS];
 
+struct device *gpio;
+struct device *gpio0;
+
 void main(void) {
 	struct device *strip;
-	struct device *gpio;
 	uint16_t counter = 0;
 
 	gpio = device_get_binding(LED_PORT);
+	gpio0 = device_get_binding("GPIO_0");
 	gpio_pin_configure(gpio, LED, GPIO_DIR_OUT);
 
 	strip = device_get_binding(STRIP_DEV_NAME);
@@ -48,8 +53,40 @@ void main(void) {
 		return;
 	}
 
+	strip_colors[0] = purple;
+	strip_colors[1] = purple;
+	strip_colors[2] = purple;
+	strip_colors[3] = purple;
+	led_strip_update_rgb(strip, strip_colors, STRIP_NUM_LEDS);
+
+	printk("Start !\n");
+	
+	struct device *display = device_get_binding(DISPLAY_DRIVER);
+	if (display == NULL) {
+		printk("Device not found\n");
+		return;
+	}
+	printk("Found!\n");
+
+	if (display_set_pixel_format(display, PIXEL_FORMAT_MONO10) != 0) {
+		printk("Failed to set required pixel format\n");
+		return;
+	}
+
+	if (cfb_framebuffer_init(display)) {
+		printk("Framebuffer initialization failed!\n");
+		return;
+	}
+
+	cfb_framebuffer_clear(display, true);
+
 	while (1) {
-		// printk("Blink %d!\n", counter++);
+		cfb_print(display,
+				      "Hello!",
+				      0, 0);
+		cfb_framebuffer_invert(display);
+		cfb_framebuffer_finalize(display);
+		printk("Blink %d!\n", counter++);
 
 		gpio_pin_write(gpio, LED, 0);
 		strip_colors[0] = red;
