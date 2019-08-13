7eca7daaae6569b093418da1eb0e57c722d66b50
Uri Shaked <uri@urishaked.com>
2019-08-13 17:51:58 +0300

    refactor(clock): use display from badgeio

--- firmware/clock/main.py
+++ firmware/clock/main.py
@@ -5,16 +5,11 @@ import rtc
 import adafruit_il0373
 import displayio
 import clock
+from badgeio import badge
 from adafruit_display_shapes.rect import Rect
 
 displayio.release_displays()
-
-spi = busio.SPI(board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-display_bus = displayio.FourWire(spi, command=board.DISP_DC, chip_select=board.DISP_CS, reset=board.DISP_RESET,
-                                 baudrate=1000000)
-time.sleep(1)
-display = adafruit_il0373.IL0373(display_bus, width=296, height=128, 
-                                 rotation=270, seconds_per_frame=5, busy_pin=board.DISP_BUSY)
+display = badge.display
 
 while True:
   rtc_instance = rtc.RTC()
