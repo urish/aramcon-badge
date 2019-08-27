40c56512b85308a8d74ec2bdba20e9eb37c6a809
Uri Shaked <uri@urishaked.com>
2019-08-27 13:42:55 +0300

    feat(firmware): add clicker demo

---
+++ firmware/clicker/README.md
@@ -0,0 +1,12 @@
+# Badge Clicker
+
+## Installation
+
+Copy [code.py](code.py) and [badgeio.py](../lib/badgeio.py) to your badge's USB Drive
+
+## Usage
+
+* Left button: Previous slide
+* Right button: Next slide
+
+Enjoy!
---
+++ firmware/clicker/code.py
@@ -0,0 +1,36 @@
+# Simple badge-based Clicker
+# Copyright (C) 2019, Uri Shaked
+#
+# Usage:
+# Left button - prev slide
+# Middle button - nothing
+# Right button - next slide
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from badgeio import badge
+import time
+
+kbd = Keyboard()
+
+while True:
+    if badge.left:
+        print("Left")
+        badge.pixels.fill((0x10, 0, 0))
+        kbd.press(Keycode.LEFT_ARROW)
+        while badge.left:
+            time.sleep(0.1)
+        kbd.release_all()
+        badge.pixels.fill(0x0)
+        
+    if badge.middle:
+        print("Middle")
+    
+    if badge.right:
+        print("Right")
+        badge.pixels.fill((0, 0, 0x10))
+        kbd.press(Keycode.RIGHT_ARROW)
+        while badge.right:
+            time.sleep(0.1)
+        kbd.release_all()
+        badge.pixels.fill(0x0)
