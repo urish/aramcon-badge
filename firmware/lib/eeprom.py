ccb8f6d5d60e963278546c9cad4dc57a667e2fb8
Uri Shaked <uri@urishaked.com>
2019-11-09 16:36:50 +0200

    feat(eeprom): multi-byte write support

--- firmware/lib/eeprom.py
+++ firmware/lib/eeprom.py
@@ -3,6 +3,8 @@
 #
 # Copyright (c) 2019 Uri Shaked
 
+import time
+
 class EEPROM:
     """
     .. code-block:: python
@@ -13,10 +15,11 @@ class EEPROM:
         print(e.read(0, 1)[0])   # will print 100
     """
 
-    def __init__(self, i2c, eeprom_addr = 0x50, eeprom_size = 256):
+    def __init__(self, i2c, eeprom_addr = 0x50, eeprom_size = 256, page_size = 16):
         self._i2c = i2c
         self._addr = eeprom_addr
         self._eeprom_size = eeprom_size
+        self._page_size = 16
 
     def _encode_addr(self, mem_addr):
         if mem_addr >= self._eeprom_size:
@@ -32,19 +35,42 @@ class EEPROM:
             pass
         try:
             buf = bytearray(size)
-            self._i2c.writeto(self._addr, bytearray(mem_addr))
+            self._i2c.writeto(self._addr, bytearray(mem_addr), stop=False)
             self._i2c.readfrom_into(self._addr, buf)
             return buf
         finally:
             self._i2c.unlock()
 
-    def write(self, mem_addr, value):
+    def _write(self, mem_addr, buf):
+        """ must write to a single page """
         mem_addr = self._encode_addr(mem_addr)
-        if value < 0 or value > 0xff:
-            raise Exception("Invalid value: {}".format(value))
         while not self._i2c.try_lock():
             pass
         try:
-            self._i2c.writeto(self._addr, bytearray(mem_addr + [value]))
+            self._i2c.writeto(self._addr, bytearray(bytes(mem_addr) + buf))
+
+            # Wait for the write to complete (polling)
+            start = time.monotonic()
+            buf = bytearray(0)
+            while time.monotonic() - start < 0.01:
+                try:
+                    self._i2c.readfrom_into(self._addr, buf)
+                    return
+                except:
+                    pass
         finally:
             self._i2c.unlock()
+
+    def write(self, mem_addr, value):
+        if value < 0 or value > 0xff:
+            raise Exception("Invalid value: {}".format(value))
+        self._write(mem_addr, bytes([value]))
+
+    def writebuf(self, mem_addr, buf):
+        page = self._page_size - (mem_addr % self._page_size)
+        while len(buf) > page:
+            self._write(mem_addr, bytes(buf[:page]))
+            mem_addr += page
+            buf = buf[page:]
+            page = 16
+        self._write(mem_addr, bytes(buf))
